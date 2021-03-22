c1aea53bcd46945da8229a6ab2f8dac74e3a72e8
Stanislav Shevtsov <ChiefService@outlook.com>
2021-03-23 01:46:58 +0300

    Update index.tsx

--- front-end-2/src/index.tsx
+++ front-end-2/src/index.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Footer from './components/footer'
+import Header from './components/header'
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
