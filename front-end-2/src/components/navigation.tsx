af968d94affd85b2975f0f87cc372aeffe73083d
Stanislav Shevtsov <ChiefService@outlook.com>
2021-04-30 23:38:06 +0300

    Update navigation.tsx

--- front-end-2/src/components/navigation.tsx
+++ front-end-2/src/components/navigation.tsx
@@ -1,26 +1,19 @@
 import './navigation.scss'
-
-import React, { useState } from "react";
-
 import { Nav } from 'react-bootstrap';
   
 function Navigation() : JSX.Element {
     return (
-    //     fxHide.lt-md="true"
-    //  fxLayout="row" fxLayoutAlign="center stretch"
-
-            <Nav activeKey="/" className="navigation">
-                <Nav.Item>
-                    <Nav.Link href="/"> Home </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link href="/projects">Projects</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link href="/login">Login</Nav.Link>
-                </Nav.Item>
-            </Nav>
-
+        <Nav className="navigation justify-content-center">
+            <Nav.Item>
+                <Nav.Link href="/"> Home </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href="/projects">Projects</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href="/login">Login</Nav.Link>
+            </Nav.Item>
+        </Nav>
     )
 }
 
